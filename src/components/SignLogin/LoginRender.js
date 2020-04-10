b1f48a4c5f837bc6405b50f39494758c99ba9472
unknown <restigjidia@outlook.com>
2020-04-10 19:11:44 +0200

    changes

--- src/components/SignLogin/LoginRender.js
+++ src/components/SignLogin/LoginRender.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
+import LogInBox from './LogInBox'
+// import SignUpBox from './SignUpBox'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import FacebookLogin from 'react-facebook-login';
 import "./LoginRender.css";
 
 // import reactDOM from 'react
@@ -28,15 +29,17 @@ class LoginRender
                 <div className="row st-row">CONNECT WITH US</div>
                 <div className="row justify-content-center nd-row">
                     <div className="col-6">
-                        <div className="text" onclick={this.showSignInBox.bind(this)}>SignIn</div>
+                        <div className="text" >SignIn</div>
                     </div>
                     <div className="col-6">
-                        <div className="text" onclick={this.showLoginBox.bind(this)}>register</div>
+                        <div className="text">register</div>
                     </div>
                 </div>
                 <div className="row rd-row">
-                    {this.state.isLoginOpen && <LoginBox />}
-                    {this.state.isSignInOpen && <SignInBox />}
+                    {/* {this.state.isLoginOpen && <LoginBox />}
+                    {this.state.isSignInOpen && <SignInBox />} */}
+                    <LogInBox />
+                    {/* <SignUpBox /> */}
                 </div>
             </div>
         )
@@ -44,193 +47,193 @@ class LoginRender
 }
 
 
-class LoginBox extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    submitLogin(e) {
+// class LoginBox extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {};
+//     }
+//     submitLogin(e) {
 
-    }
-    render() {
-        return (
-            // <section className="container-fluid">
-            <section className="row justify-content-center">
-                <form className="form-horizontal form-container">
-                    <div className="form-group">
-                        <label htmlFor="inputEmail" className="col-sm-2 control-label">Email</label>
-                        <div className="col-sm-12">
-                            <input type="email" className="form-control" id="inputEmail" placeholder="Email" />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="inputPassword" className="col-sm-2 control-label">Password</label>
-                        <div className="col-sm-12">
-                            <input type="password" className="form-control" id="inputPassword" placeholder="Password" />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="col-sm-offset-2 col-sm-12">
-                            <div className="checkbox">
-                                <label>
-                                    <input type="checkbox" /> Remember me
-                                        </label>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="col-sm-offset-2 col-sm-12">
-                            <button type="submit" className="btn  btn-block" onClick={this.submitLogin.bind(this)}>Log in</button>
-                        </div>
-                    </div>
-                </form>
-            </section>
-            // </section>
-        )
-    }
-}
+//     }
+//     render() {
+//         return (
+//             // <section className="container-fluid">
+//             <section className="row justify-content-center">
+//                 <form className="form-horizontal form-container">
+//                     <div className="form-group">
+//                         <label htmlFor="inputEmail" className="col-sm-2 control-label">Email</label>
+//                         <div className="col-sm-12">
+//                             <input type="email" className="form-control" id="inputEmail" placeholder="Email" />
+//                         </div>
+//                     </div>
+//                     <div className="form-group">
+//                         <label htmlFor="inputPassword" className="col-sm-2 control-label">Password</label>
+//                         <div className="col-sm-12">
+//                             <input type="password" className="form-control" id="inputPassword" placeholder="Password" />
+//                         </div>
+//                     </div>
+//                     <div className="form-group">
+//                         <div className="col-sm-offset-2 col-sm-12">
+//                             <div className="checkbox">
+//                                 <label>
+//                                     <input type="checkbox" /> Remember me
+//                                         </label>
+//                             </div>
+//                         </div>
+//                     </div>
+//                     <div className="form-group">
+//                         <div className="col-sm-offset-2 col-sm-12">
+//                             <button type="submit" className="btn  btn-block" onClick={this.submitLogin.bind(this)}>Log in</button>
+//                         </div>
+//                     </div>
+//                 </form>
+//             </section>
+//             // </section>
+//         )
+//     }
+// }
 
-class SignInBox extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-    submitLogin(e) {
-    }
-    render() {
-        return (
-            <section className="container-fluid">
-                <section className="row justify-content-center">
-                    <form className="form-horizontal form-container">
-                        <div className="form-group">
-                            <label htmlFor="inputEmail" className="col-sm-2 control-label">Email</label>
-                            <div className="col-sm-12">
-                                <input type="email" className="form-control" id="inputEmail" placeholder="Email" />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="inputPassword" className="col-sm-2 control-label">Password</label>
-                            <div className="col-sm-12">
-                                <input type="password" className="form-control" id="inputPassword" placeholder="Password" />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <div className="col-sm-offset-2 col-sm-12">
-                                <div className="checkbox">
-                                    <label>
-                                        <input type="checkbox" /> Remember me
-                                        </label>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <div className="col-sm-offset-2 col-sm-12">
-                                <button type="submit" className="btn  btn-block" onClick={this.submitLogin.bind(this)}>Log in</button>
-                            </div>
-                        </div>
-                    </form>
-                </section>
-            </section>
-        )
-    }
-}
+// class SignInBox extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {};
+//     }
+//     submitLogin(e) {
+//     }
+//     render() {
+//         return (
+//             <section className="container-fluid">
+//                 <section className="row justify-content-center">
+//                     <form className="form-horizontal form-container">
+//                         <div className="form-group">
+//                             <label htmlFor="inputEmail" className="col-sm-2 control-label">Email</label>
+//                             <div className="col-sm-12">
+//                                 <input type="email" className="form-control" id="inputEmail" placeholder="Email" />
+//                             </div>
+//                         </div>
+//                         <div className="form-group">
+//                             <label htmlFor="inputPassword" className="col-sm-2 control-label">Password</label>
+//                             <div className="col-sm-12">
+//                                 <input type="password" className="form-control" id="inputPassword" placeholder="Password" />
+//                             </div>
+//                         </div>
+//                         <div className="form-group">
+//                             <div className="col-sm-offset-2 col-sm-12">
+//                                 <div className="checkbox">
+//                                     <label>
+//                                         <input type="checkbox" /> Remember me
+//                                         </label>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                         <div className="form-group">
+//                             <div className="col-sm-offset-2 col-sm-12">
+//                                 <button type="submit" className="btn  btn-block" onClick={this.submitLogin.bind(this)}>Log in</button>
+//                             </div>
+//                         </div>
+//                     </form>
+//                 </section>
+//             </section>
+//         )
+//     }
+// }
 
-class RegisterBox extends Component {
-    state = {
-        isLoggedIn: false,
-        userId: '',
-        name: '',
-        email: '',
-        picture: ''
-    }
-    submitRegister(e) {
-    }
-    responseFacebook = response => {
-        // this.setState({
-        //   isLoggedIn: true,
-        //   userId: response.userId,
-        //   name: response.name,
-        //   email: response.email,
-        //   picture: response.picture.data.url
-        // });
-        console.log(response);
-    };
-    componentClicked = () => console.log("clicked");
+// class RegisterBox extends Component {
+//     state = {
+//         isLoggedIn: false,
+//         userId: '',
+//         name: '',
+//         email: '',
+//         picture: ''
+//     }
+//     submitRegister(e) {
+//     }
+//     responseFacebook = response => {
+//         // this.setState({
+//         //   isLoggedIn: true,
+//         //   userId: response.userId,
+//         //   name: response.name,
+//         //   email: response.email,
+//         //   picture: response.picture.data.url
+//         // });
+//         console.log(response);
+//     };
+//     componentClicked = () => console.log("clicked");
 
-    responseGoogle = response => {
-        console.log(response);
-    };
-    render() {
+//     responseGoogle = response => {
+//         console.log(response);
+//     };
+//     render() {
 
-        let fbContent, googleContent;
+//         let fbContent, googleContent;
 
-        if (this.state.isLoggedIn) {
-            // googleContent = null;
-            fbContent = null;
-        } else {
-            // googleContent = (<GoogleLogin
-            //   clientId="167174526204-4j9traaqfnh7ovugg5q9rvghe3l8npg8.apps.googleusercontent.com"
-            //   autoLoad={true}
-            //   buttonText="Login"
-            //   onSuccess={this.responseGoogle}
-            //   onFailure={this.responseGoogle}
-            //   cookiePolicy={'single_host_origin'}
-            // />)
+//         if (this.state.isLoggedIn) {
+//             // googleContent = null;
+//             fbContent = null;
+//         } else {
+//             // googleContent = (<GoogleLogin
+//             //   clientId="167174526204-4j9traaqfnh7ovugg5q9rvghe3l8npg8.apps.googleusercontent.com"
+//             //   autoLoad={true}
+//             //   buttonText="Login"
+//             //   onSuccess={this.responseGoogle}
+//             //   onFailure={this.responseGoogle}
+//             //   cookiePolicy={'single_host_origin'}
+//             // />)
 
-            fbContent = (<FacebookLogin
-                appId="513613722875059"
-                autoLoad={true}
-                fields="name,email,picture"
-                onClick={this.componentClicked}
-                callback={this.responseFacebook} />);
-        }
-        return (
-            <section className="container-fluid">
-                <section className="row justify-content-center">
-                    <form className="form-horizontal form-container">
-                        <div className="form-group">
-                            <label htmlFor="name" className="col-sm-2 control-label">Name</label>
-                            <div className="col-sm-12">
-                                <input type="text" className="form-control" id="name" placeholder="Name" />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="inputEmail3" className="col-sm-2 control-label">Email</label>
-                            <div className="col-sm-12">
-                                <input type="email" className="form-control" id="inputEmail3" placeholder="Email" />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="inputPassword3" className="col-sm-2 control-label">Password</label>
-                            <div className="col-sm-12">
-                                <input type="password" className="form-control" id="inputPassword3" placeholder="Password" />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="confirmPassword" className="col-sm-2 control-label">Confirm Password</label>
-                            <div className="col-sm-12">
-                                <input type="password" className="form-control" id="confirmPassword" placeholder="Password" />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <div className="col-sm-offset-2 col-sm-12">
-                                <button type="submit" className="btn  btn-block" onClick={this.submitRegister.bing(this)}>Sign in</button>
-                            </div>
-                        </div>
-                        <div className="form-group break-line">
-                            <div className="horizontal-rule"></div>
-                            <div id="text">or</div>
-                        </div>
-                        <div className="login">
-                            <div className="row facebook justify-content-center">{fbContent}</div>
-                            {/* <div className="google">{googleContent}</div> */}
-                        </div>
-                    </form>
-                </section>
-            </section>
-        );
-    }
-}
+//             fbContent = (<FacebookLogin
+//                 appId="513613722875059"
+//                 autoLoad={true}
+//                 fields="name,email,picture"
+//                 onClick={this.componentClicked}
+//                 callback={this.responseFacebook} />);
+//         }
+//         return (
+//             <section className="container-fluid">
+//                 <section className="row justify-content-center">
+//                     <form className="form-horizontal form-container">
+//                         <div className="form-group">
+//                             <label htmlFor="name" className="col-sm-2 control-label">Name</label>
+//                             <div className="col-sm-12">
+//                                 <input type="text" className="form-control" id="name" placeholder="Name" />
+//                             </div>
+//                         </div>
+//                         <div className="form-group">
+//                             <label htmlFor="inputEmail3" className="col-sm-2 control-label">Email</label>
+//                             <div className="col-sm-12">
+//                                 <input type="email" className="form-control" id="inputEmail3" placeholder="Email" />
+//                             </div>
+//                         </div>
+//                         <div className="form-group">
+//                             <label htmlFor="inputPassword3" className="col-sm-2 control-label">Password</label>
+//                             <div className="col-sm-12">
+//                                 <input type="password" className="form-control" id="inputPassword3" placeholder="Password" />
+//                             </div>
+//                         </div>
+//                         <div className="form-group">
+//                             <label htmlFor="confirmPassword" className="col-sm-2 control-label">Confirm Password</label>
+//                             <div className="col-sm-12">
+//                                 <input type="password" className="form-control" id="confirmPassword" placeholder="Password" />
+//                             </div>
+//                         </div>
+//                         <div className="form-group">
+//                             <div className="col-sm-offset-2 col-sm-12">
+//                                 <button type="submit" className="btn  btn-block" onClick={this.submitRegister.bing(this)}>Sign in</button>
+//                             </div>
+//                         </div>
+//                         <div className="form-group break-line">
+//                             <div className="horizontal-rule"></div>
+//                             <div id="text">or</div>
+//                         </div>
+//                         <div className="login">
+//                             <div className="row facebook justify-content-center">{fbContent}</div>
+//                             {/* <div className="google">{googleContent}</div> */}
+//                         </div>
+//                     </form>
+//                 </section>
+//             </section>
+//         );
+//     }
+// }
 
 
 export default LoginRender
---
+++ src/components/SignLogin/SignUpBox.js
@@ -0,0 +1,104 @@
+import React, { Component } from "react";
+import FacebookLogin from 'react-facebook-login';
+import "./signup.css";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+class SignUpBox extends Component {
+  state = {
+    isLoggedIn: false,
+    userId: '',
+    name: '',
+    email: '',
+    picture: ''
+  }
+  submitRegister(e) {
+  }
+  responseFacebook = response => {
+    // this.setState({
+    //   isLoggedIn: true,
+    //   userId: response.userId,
+    //   name: response.name,
+    //   email: response.email,
+    //   picture: response.picture.data.url
+    // });
+    console.log(response);
+  };
+  componentClicked = () => console.log("clicked");
+
+  responseGoogle = response => {
+    console.log(response);
+  };
+  render() {
+
+    let fbContent, googleContent;
+
+    if (this.state.isLoggedIn) {
+      // googleContent = null;
+      fbContent = null;
+    } else {
+      // googleContent = (<GoogleLogin
+      //   clientId="167174526204-4j9traaqfnh7ovugg5q9rvghe3l8npg8.apps.googleusercontent.com"
+      //   autoLoad={true}
+      //   buttonText="Login"
+      //   onSuccess={this.responseGoogle}
+      //   onFailure={this.responseGoogle}
+      //   cookiePolicy={'single_host_origin'}
+      // />)
+
+      fbContent = (<FacebookLogin
+        appId="513613722875059"
+        autoLoad={true}
+        fields="name,email,picture"
+        onClick={this.componentClicked}
+        callback={this.responseFacebook} />);
+    }
+    return (
+      <section className="container-fluid">
+        <section className="row justify-content-center">
+          <form className="form-horizontal form-container">
+            <div className="form-group">
+              <label htmlFor="name" className="col-sm-2 control-label">Name</label>
+              <div className="col-sm-12">
+                <input type="text" className="form-control" id="name" placeholder="Name" />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="inputEmail3" className="col-sm-2 control-label">Email</label>
+              <div className="col-sm-12">
+                <input type="email" className="form-control" id="inputEmail3" placeholder="Email" />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="inputPassword3" className="col-sm-2 control-label">Password</label>
+              <div className="col-sm-12">
+                <input type="password" className="form-control" id="inputPassword3" placeholder="Password" />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="confirmPassword" className="col-sm-2 control-label">Confirm Password</label>
+              <div className="col-sm-12">
+                <input type="password" className="form-control" id="confirmPassword" placeholder="Password" />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="col-sm-offset-2 col-sm-12">
+                <button type="submit" className="btn  btn-block" onClick={this.submitRegister.bing(this)}>Sign in</button>
+              </div>
+            </div>
+            <div className="form-group break-line">
+              <div className="horizontal-rule"></div>
+              <div id="text">or</div>
+            </div>
+            <div className="login">
+              <div className="row facebook justify-content-center">{fbContent}</div>
+              {/* <div className="google">{googleContent}</div> */}
+            </div>
+          </form>
+        </section>
+      </section>
+    );
+  }
+}
+
+
+export default SignUpBox
