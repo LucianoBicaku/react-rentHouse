5f09e52314c7a390ecf283f5dcaaf9f586582b02
Luciano Bicaku <luciano.bicaku@gmail.com>
2020-04-12 18:06:36 +0200

    mbarova template ne html/css

--- src/components/SignLogin/LoginRender.js
+++ src/components/SignLogin/LoginRender.js
@@ -1,47 +1,49 @@
-import React, { Component } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./LoginRender.css";
-import SignUpBox from './SignUpBox'
-import LogInBox from './LogInBox';
+import SignUpBox from "./SignUpBox";
+import LogInBox from "./LogInBox";
 
-
-class LoginRender
-    extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLogInOpen: true,
-            isSignInOpen: false
-        };
-    }
-    showLogInBox() {
-        this.setState({ isLogInOpen: true, isSignInOpen: false })
-    }
-    showSignInBox() {
-        this.setState({ isSignInOpen: true, isLogInOpen: false })
-    }
-    render() {
-        return (
-            <div className="container my-container">
-                <div className="row st-row">
-                    <div className="text">CONNECT WITH US</div>
-                    <button className="close">&#x2716;</button>
-                </div>
-                <div className="row justify-content-center nd-row">
-                    <div className="col-6">
-                        <div className="logintext" onClick={this.showLogInBox.bind(this)} >Login</div>
-                    </div>
-                    <div className="col-6">
-                        <div className="signuptext" onClick={this.showSignInBox.bind(this)}>Sign Up</div>
-                    </div>
-                </div>
-                <div className="row rd-row">
-                    {this.state.isLogInOpen && <LogInBox />}
-                    {this.state.isSignInOpen && <SignUpBox />}
-                </div>
+class LoginRender extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogInOpen: true,
+      isSignInOpen: false,
+    };
+  }
+  showLogInBox() {
+    this.setState({ isLogInOpen: true, isSignInOpen: false });
+  }
+  showSignInBox() {
+    this.setState({ isSignInOpen: true, isLogInOpen: false });
+  }
+  render() {
+    return (
+      <div className="container my-container">
+        <div className="row st-row">
+          <div className="text">CONNECT WITH US</div>
+          <button className="close">&#x2716;</button>
+        </div>
+        <div className="row justify-content-center nd-row">
+          <div className="col-6">
+            <div className="logintext" onClick={this.showLogInBox.bind(this)}>
+              Login
             </div>
-        )
-    }
+          </div>
+          <div className="col-6">
+            <div className="signuptext" onClick={this.showSignInBox.bind(this)}>
+              Sign Up
+            </div>
+          </div>
+        </div>
+        <div className="row rd-row">
+          {this.state.isLogInOpen && <LogInBox />}
+          {this.state.isSignInOpen && <SignUpBox />}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default LoginRender
+export default LoginRender;
