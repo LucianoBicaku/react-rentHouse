7b6b8c5d439492136e2ab8b0765f97c2d541b7e8
Luciano Bicaku <luciano.bicaku@gmail.com>
2020-04-16 15:40:57 +0200

    lol

--- src/App.js
+++ src/App.js
@@ -16,9 +16,6 @@ function App() {
     <>
       <Router>
         <Switch>
-          <Route exact path="/">
-            <LoginRender />
-          </Route>
           <Route exact path="/about">
             <AboutUs />
           </Route>
