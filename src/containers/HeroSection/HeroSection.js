568fb846c9f447ed3a167eeeda3e0080a5c57f9a
Luciano Bicaku <luciano.bicaku@gmail.com>
2020-05-10 19:05:25 +0200

    commit

--- src/containers/HeroSection/HeroSection.js
+++ src/containers/HeroSection/HeroSection.js
@@ -40,13 +40,12 @@ export default function HeroSection() {
         <p>Give us the clues and get a house to choose!</p>
       </div>
       <div className="item2">
-        <form>
-          <label className="location">
-            <i>Location</i>
-            <br />
+        <div className="location">
+          <label htmlFor="location">Location</label>
+          <div className="location-content">
             <input
-              type="text"
-              name="name"
+              type="number"
+              name="location"
               placeholder="Location"
               onChange={(event) => {
                 setLocation(event.target.value);
@@ -57,11 +56,13 @@ export default function HeroSection() {
                 <i className="fas fa-street-view"></i>
               </button>
             </Link>
-          </label>
-          <label className="price">
-            <i>Price Range</i>
-            <br />
-            <div>
+          </div>
+        </div>
+
+        <div className="price">
+          <label htmlFor="price">Price Range</label>
+          <div className="price-content">
+            <div className="price-input">
               <input
                 type="text"
                 name="price"
@@ -84,11 +85,9 @@ export default function HeroSection() {
                 onChange={(event) => {
                   const newValue = { min: price.min, max: event.target.value };
                   setPrice(newValue);
-                  // console.log(price.max);
                 }}
               />
             </div>
-            <br />
 
             <InputRange
               step={100}
@@ -97,49 +96,54 @@ export default function HeroSection() {
               formatLabel={(value) => `${value} All`}
               value={price}
               onChange={(value) => setPrice(value)}
-            // onChangeComplete={(value) => {
-            //   console.log(value);
-            // }}
+              // onChangeComplete={(value) => {
+              //   console.log(value);
+              // }}
             />
-          </label>
-          <div className="R-input">
-            <label>
-              Rooms
-              <br />
-              <select
-                value={rooms}
-                onChange={(event) => {
-                  setRooms(event.target.value);
-                }}
-              >
-                <option value={1}>1</option>
-                <option value={2}>2</option>
-                <option value={3}>3</option>
-                <option value={0}>More</option>
-              </select>
-            </label>
-            <label>
-              Roomates
-              <br />
-              <select>
-                <option value={1}>1</option>
-                <option value={2}>2</option>
-                <option value={3}>3</option>
-                <option value={0}>More</option>
-              </select>
-            </label>
           </div>
-        </form>
-        <button
-          onClick={() => {
-            console.log(location);
-            console.log(price);
-            console.log(parseInt(rooms, 10));
-            search(price.min, price.max, location, parseInt(rooms, 10));
-          }}
-        >
-          Search
-        </button>
+        </div>
+
+        <div className="R-input">
+          <label>
+            Rooms
+            <br />
+            <select
+              value={rooms}
+              onChange={(event) => {
+                setRooms(event.target.value);
+              }}
+            >
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={0}>More</option>
+            </select>
+          </label>
+          <label>
+            Roomates
+            <br />
+            <select>
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={0}>More</option>
+            </select>
+          </label>
+        </div>
+
+        <div className="search-btn-container">
+          <button
+            className="item2-search-button"
+            onClick={() => {
+              console.log(location);
+              console.log(price);
+              console.log(parseInt(rooms, 10));
+              search(price.min, price.max, location, parseInt(rooms, 10));
+            }}
+          >
+            Search
+          </button>
+        </div>
       </div>
     </div>
   );
