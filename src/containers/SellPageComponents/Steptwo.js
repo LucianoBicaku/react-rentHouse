ff05d4e164d3b9c68be5361488295ad0ba71ebc5
unknown <restigjidia@outlook.com>
2020-05-17 02:49:15 +0200

    changes

---
+++ src/containers/SellPageComponents/Steptwo.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import Checkbox from './Checkbox'
+
+class Steptwo extends Component {
+    state = {
+        eletricity: false
+    }
+    handleChange = input => evt => {
+        if (evt.target.type === 'checkbox') {
+            // console.log(evt.target.checked)
+            this.setState({ input: evt.target.checked });
+
+        }
+        else {
+            this.setState({ input: evt.target.value });
+            console.log(this.state.eletricity)
+        }
+
+    }
+    render() {
+        return (
+            <div className="row justify-content-center">
+                <div className="house-details-container row">
+                    <h5>Is any of the following included in your price?</h5>
+                    <Checkbox handleChange={this.props.handleChange} stateName="dritat" labelName="Electricity" />
+                    <Checkbox handleChange={this.props.handleChange} stateName="uji" labelName="Water" />
+                    <Checkbox handleChange={this.props.handleChange} stateName="internet" labelName="Internet" />
+
+                    <h5>Number of rooms</h5>
+                    <div className="form-group">
+                        <label htmlFor="bedrooms">Bedrooms</label>
+                        <input type="number" min="0" className="form-control" id="bedrooms" onChange={this.props.handleChange('nr_dhomash')} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="bathrooms">Bathrooms</label>
+                        <input type="number" min="0" className="form-control" id="bathrooms" onChange={this.props.handleChange('nr_banjosh')} />
+                    </div>
+                    <div className="form-group block-element no-of-people">
+                        <h5>How many people do you want to live in this house?</h5>
+                        <input type="number" min="0" className="form-control" onChange={this.props.handleChange('nr_personash')} />
+                    </div>
+
+                    <h5>Extra Features</h5>
+                    <Checkbox handleChange={this.props.handleChange} stateName="parkim" labelName="Parking" />
+                    <Checkbox handleChange={this.props.handleChange} stateName="ashensor" labelName="Elevator" />
+                    <Checkbox handleChange={this.props.handleChange} stateName="wifi" labelName="Wi-fi" />
+                    <Checkbox handleChange={this.props.handleChange} stateName="kondicioner" labelName="Air-conditioning" />
+                    <Checkbox handleChange={this.props.handleChange} stateName="kafshe" labelName="Pets allowed" />
+                    <Checkbox handleChange={this.props.handleChange} stateName="ballkon" labelName="Balcony" />
+                    <Checkbox handleChange={this.props.handleChange} stateName="kopsht" labelName="Garden" />
+                    <Checkbox handleChange={this.props.handleChange} stateName="sendeGatimi" labelName="Kitchenware" />
+                    <Checkbox handleChange={this.props.handleChange} stateName="televizor" labelName="TV" />
+
+                    <div className="form-group block-element">
+                        <label className="block-element" for="exampleFormControlTextarea1">Description</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="20"></textarea>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Steptwo
--- src/pages/SellPage.js
+++ src/pages/SellPage.js
@@ -1,90 +1,80 @@
 import React, { Component } from 'react'
 import DarkHeader from "../containers/global_components/Header/DarkHeader";
 import SellPageWallpaper from "../containers/SellPageWallpaper/SellPageWallpaper";
-import "../containers/SellPageComponents/sellpage.css"
+import "../containers/SellPageComponents/sellpage.css";
+import Stepone from "../containers/SellPageComponents/Stepone"
+import Steptwo from "../containers/SellPageComponents/Steptwo"
+
 export class SellPage extends Component {
     state = {
         address: '',
-        surface: '',
-        noOfRooms: '',
-        typeOfProperty: '',
-        floorOfProperty: '',
-        price: ''
+        sip: '',
+        lloji: '',
+        kati: '',
+        cmimi: '',
+        uji: false,
+        dritat: false,
+        internet: false,
+        nr_dhomash: null,
+        nr_banjosh: null,
+        nr_personash: null,
+        parkim: false,
+        ashensor: false,
+        wifi: false,
+        kondicioner: false,
+        kafshe: false,
+        ballkon: false,
+        kopsht: false,
+        sendeGatimi: false,
+        televizor: false,
+        step: 1
     }
-    onAddressChange(e) {
-        this.setState({ address: e.target.value });
-    }
-    onSurfaceChange(e) {
-        this.setState({ surface: e.target.value });
-    }
-    onNoOfRoomsChange(e) {
-        this.setState({ noOfRooms: e.target.value });
-    }
-    onTypeOfPropertyChange(e) {
-        this.setState({ typeOfProperty: e.target.value });
-    }
-    onFloorOfPropertyChange(e) {
-        this.setState({ floorOfProperty: e.target.value });
-    }
-    onPriceChange(e) {
-        this.setState({ price: e.target.value });
+    handleChange = input => evt => {
+        if (evt.target.type === 'checkbox') {
+            this.setState({ [input]: evt.target.checked });
+            console.log(input);
+            console.log(this.state.wifi);
+            console.log(this.state.kopsht);
+            console.log(this.state.parkim);
+            console.log(this.state.televizor);
+        }
+        else {
+            this.setState({ [input]: evt.target.value });
+            console.log(input);
+            console.log(this.state.wifi);
+        }
+
     }
     render() {
         return (
             <>
                 <DarkHeader />
                 <SellPageWallpaper />
-                <div className="container-fluid">
-                    <div className="row justify-content-center">
-                        <div className="text">Add your house now!</div>
-                    </div>
-                    <div className="row justify-content-center">
-                        <div className="steps-container row">
-                            <div className="step1 col-2">
-                                <div className="dot1">1</div>
-                            </div>
-                            <div className="first-dot-tittle">Peronal information</div>
-                            <div className="line"></div>
-                            <div className="space col-8"></div>
-                            <div className="step2 col-2">
-                                <div className="dot2">2</div>
-                            </div>
-                            <div className="nd-dot-tittle">Upload Pictures</div>
-                        </div>
-                    </div>
-                    <form className="sell-form">
-                        <div className="row justify-content-center">
-                            <div className="left-fields">
-                                <div className="form-group">
-                                    <label htmlFor="addres">Address</label>
-                                    <input type="text" className="form-control" id="addres" onChange={this.onAddressChange} /></div>
-                                <div className="form-group">
-                                    <label htmlFor="surface">Surface(m<small><sup>2</sup></small>)</label>
-                                    <input type="text" className="form-control" id="surface" />onChange={this.onSurfaceChange}</div>
-                                <div className="form-group">
-                                    <label htmlFor="no-of-rooms">Number of rooms</label>
-                                    <input type="text" className="form-control" id="no-of-rooms" onChange={this.onNoOfRoomsChange} /></div>
-                            </div>
-                            <div className="right-fields">
-                                <div className="form-group">
-                                    <label htmlFor="typeofproperty">Type of property</label>
-                                    <input type="text" className="form-control" id="typeofproperty" onChange={this.onTypeOfPropertyChange} />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="surface">Floor of property</label>
-                                    <input type="text" className="form-control" id="surface" onChange={this.onFloorOfPropertyChange} />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="no-of-rooms">Price(per month)</label>
-                                    <input type="text" className="form-control" id="no-of-rooms" onChange={this.onNOOfRoomsChange()} /></div>
-                            </div>
-                        </div>
-                        <div className="row justify-content-center">
-                            <div className="btn">Next</div>
-                        </div>
-                    </form>
-
+                <div className="row justify-content-center">
+                    <div className="text">Add your house now!</div>
                 </div>
+                <div className="row justify-content-center">
+                    <div className="steps-container row justify-content-center">
+                        <div className="step1">
+                            <div className="dot1">1</div>
+                        </div>
+                        <div className="first-dot-tittle">House Information</div>
+                        <div className="line"></div>
+                        <div className="space col-4"></div>
+                        <div className="step2">
+                            <div className="dot2">2</div>
+                        </div>
+                        <div className="space col-4"></div>
+                        <div className="nd-dot-tittle">House Details</div>
+                        <div className="step3">
+                            <div className="dot3">3</div>
+                        </div>
+                        <div className="rd-dot-tittle">third text</div>
+                    </div>
+                </div>
+                {/* <Stepone handleChange={this.handleChange} /> */}
+                <Steptwo handleChange={this.handleChange} />
+
             </>
         )
     }
